cd34d4724e5125c00820312946b4a3bae12fa07d
LuisVFCO <luiz2001vine@hotmail.com>
2025-08-15 20:57:09 -0300

    Adding the fourth lesson and its activities

---
+++ AtvAula4.py
@@ -0,0 +1,13 @@
+usuNome = input("Digite o seu nome: ")
+usuSenha = int(input("Digite a sua senha: "))
+
+nome = "luis"
+senha = 12345678
+
+if usuNome == nome and usuSenha == senha:
+    print("Nome e senha validados")
+
+else:
+    print(" ERRO!!! Tente Novamente ")
+
+
