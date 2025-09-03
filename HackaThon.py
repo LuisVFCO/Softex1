2ef410a2dd9ce1095d5fc61343133d0e095a6088
LuisVFCO <luiz2001vine@hotmail.com>
2025-09-03 20:52:09 -0300

    final project

--- HackaThon.py
+++ HackaThon.py
@@ -1,19 +1,30 @@
-caixa = str(input("Deseja registrar um novo cliente (s/n)?: ")).lower()
+print("CINEMA - PREÇOS")
+print("Preço infantil: R$ 15,00")
+print("Preço estudante: R$ 15,00")
+print("Preço normal: R$ 30,00")
+print("Segunta tem promoção!! Ingresso por R$12,50")
+
+dia = input("Que dia da semana é hoje (segunda): ").lower()
+caixa = input("Deseja registrar um novo cliente (s/n)?: ").lower()
 
 totalingresso = []
 
 while caixa == "s":
     totalingresso.append(caixa)
-    estu = input("Você é estudante? (s/n)?: ")
 
-    if estu == "s":
-        print("Preço estudante: R$ 15,00")
+    if dia == "segunda":
+        print("Preço promocional: R$ 12,50")
     else:
-        idade = int(input("Digite sua idade: "))
-        if idade < 12:
-            print("Preço infantil: R$ 15,00")
+        estu = input("Você é estudante? (s/n)?: ").lower()
+        if estu == "s":
+            print("Preço estudante: R$ 15,00")
         else:
-            print("Preço normal: R$ 30,00")
-    caixa = str(input("Deseja registrar um novo cliente (s/n)?: ")).lower()
+            idade = int(input("Digite sua idade: "))
+            if idade < 12:
+                print("Preço infantil: R$ 15,00")
+            else:
+                print("Preço normal: R$ 30,00")
 
-print("Total de ingressos vendidos: ", len(totalingresso))
+    caixa = input("Deseja registrar um novo cliente (s/n)?: ").lower()
+
+print("Total de ingressos vendidos:", len(totalingresso))
