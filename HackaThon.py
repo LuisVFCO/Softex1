6a9a8b983cea692e05ccf4a9f5a924c7a80d4f11
LuisVFCO <luiz2001vine@hotmail.com>
2025-09-03 19:29:02 -0300

    Sprint 2

--- HackaThon.py
+++ HackaThon.py
@@ -1,7 +1,11 @@
 idade = int(input("Digite sua idade: "))
+estu = input("Você é estudante? (s/n)?: ")
 
-if idade < 12:
-    print("Preço infantil: R$ 15,00")
-elif idade >= 12:
-    print("Preço normal: R$ 30,00")
+if estu == "s":
+    print("Preço estudante: R$ 15,00")
+else:
+    if idade < 12:
+        print("Preço infantil: R$ 15,00")
+    elif idade >= 12:
+        print("Preço normal: R$ 30,00")
     
