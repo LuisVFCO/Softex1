90a711370479228f6f08995f8e49f898793b7539
LuisVFCO <luiz2001vine@hotmail.com>
2025-09-03 20:35:13 -0300

    dj

--- HackaThon.py
+++ HackaThon.py
@@ -1,11 +1,19 @@
-idade = int(input("Digite sua idade: "))
-estu = input("Você é estudante? (s/n)?: ")
+caixa = str(input("Deseja registrar um novo cliente (s/n)?: ")).lower()
 
-if estu == "s":
-    print("Preço estudante: R$ 15,00")
-else:
-    if idade < 12:
-        print("Preço infantil: R$ 15,00")
-    elif idade >= 12:
-        print("Preço normal: R$ 30,00")
-    
+totalingresso = []
+
+while caixa == "s":
+    totalingresso.append(caixa)
+    estu = input("Você é estudante? (s/n)?: ")
+
+    if estu == "s":
+        print("Preço estudante: R$ 15,00")
+    else:
+        idade = int(input("Digite sua idade: "))
+        if idade < 12:
+            print("Preço infantil: R$ 15,00")
+        else:
+            print("Preço normal: R$ 30,00")
+    caixa = str(input("Deseja registrar um novo cliente (s/n)?: ")).lower()
+
+print("Total de ingressos vendidos: ", len(totalingresso))
